c23e3b6c2f818e54741510ef00b9d4fdbd6e008b
Celestine Ekoh-Ordan <CEO.ehis@outlook.com>
2019-04-07 19:12:45 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View>
+        <Text>Welcome to React Native!</Text>
+      </View>
+    );
+  }
+}
