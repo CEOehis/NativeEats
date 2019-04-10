ebeb52c94cf9f442d10e6063607c583aa0864017
Celestine Ekoh-Ordan <CEO.ehis@outlook.com>
2019-04-10 02:41:58 +0100

    update status bar background on android

--- src/components/Header.android.js
+++ src/components/Header.android.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
-import { Text } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
 
 import HeaderStyle from 'styles/HeaderStyle';
 
 export default class Header extends Component {
   render() {
-    return <Text style={HeaderStyle.header}>Restaurants</Text>;
+    return (
+      <View>
+        <StatusBar backgroundColor="#333" barStyle="light-content" />
+        <Text style={HeaderStyle.header}>Restaurants</Text>
+      </View>
+    );
   }
 }
